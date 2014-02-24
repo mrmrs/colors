ecc420632224c86a14ad4a1435806c1a2e0c9b81
Dylan Baskind <dylan.baskind@gmail.com>
2014-02-24 12:27:22 +1100

    Added CoffeeScript + Javascript versions

---
+++ javascript.js
@@ -0,0 +1,56 @@
+var colors;
+
+colors = [
+  {
+    name: "Aqua",
+    color: "#7FDBFF"
+  }, {
+    name: "Blue",
+    color: "#0074D9"
+  }, {
+    name: "Navy",
+    color: "#001F3F"
+  }, {
+    name: "Teal",
+    color: "#39CCCC"
+  }, {
+    name: "Green",
+    color: "#2ECC40"
+  }, {
+    name: "Olive",
+    color: "#3D9970"
+  }, {
+    name: "Lime",
+    color: "#01FF70"
+  }, {
+    name: "Yellow",
+    color: "#FFDC00"
+  }, {
+    name: "Orange",
+    color: "#FF851B"
+  }, {
+    name: "Red",
+    color: "#FF4136"
+  }, {
+    name: "Fuchsia",
+    color: "#F012BE"
+  }, {
+    name: "Purple",
+    color: "#B10DC9"
+  }, {
+    name: "Maroon",
+    color: "#85144B"
+  }, {
+    name: "White",
+    color: "#fff"
+  }, {
+    name: "Silver",
+    color: "#ddd"
+  }, {
+    name: "Gray",
+    color: "#aaa"
+  }, {
+    name: "Black",
+    color: "#111"
+  }
+];
