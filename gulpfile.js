d477aafe2f30854e5dad3887b1a1296323c55607
Ilan Biala <ilan.biala@gmail.com>
2014-03-10 23:04:49 -0400

    Organized Gulpfile. Added CoffeeScript compilation.

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,127 @@
 // Load plugins
 
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    watch = require('gulp-watch'),
-    lr    = require('tiny-lr'),
-    server = lr(),
-    livereload = require('gulp-livereload'),
-    prefix = require('gulp-autoprefixer'),
-    minifyCSS = require('gulp-minify-css'),
-    sass = require('gulp-sass'),
-    myth = require('gulp-myth'),
-    stylus = require('gulp-stylus'),
-    csslint = require('gulp-csslint');
+	gutil = require('gulp-util'),
+	watch = require('gulp-watch'),
+	lr = require('tiny-lr'),
+	server = lr(),
+	livereload = require('gulp-livereload'),
+	prefix = require('gulp-autoprefixer'),
+	minifyCSS = require('gulp-minify-css'),
+	sass = require('gulp-sass'),
+	myth = require('gulp-myth'),
+	stylus = require('gulp-stylus'),
+	csslint = require('gulp-csslint'),
+	rename = require('gulp-rename'),
+	less = require('gulp-less'),
+	coffee = require('gulp-coffee');
 
+// CoffeeScript
 
-// Task to minify all css files in the css directory
-
-gulp.task('minify', function(){
-  gulp.src('./css/*.css')
-    .pipe(minifyCSS({keepSpecialComments: 0}))
-    .pipe(gulp.dest('./css/'));
+gulp.task('coffee', function () {
+	gulp.src('./coffee/*.coffee')
+		.pipe(coffee({
+			bare: true
+		})).on('error', gutil.log)
+		.pipe(gulp.dest('./coffee'));
 });
 
+// CSS
 
 // Use csslint without box-sizing or compatible vendor prefixes (these
 // don't seem to be kept up to date on what to yell about)
 
-gulp.task('lint', function(){
-  gulp.src('./css/*.css')
-    .pipe(csslint({
-          'compatible-vendor-prefixes': false,
-          'box-sizing': false
-        }))
-    .pipe(csslint.reporter());
-
+gulp.task('lint', function () {
+	gulp.src('./css/*.css')
+		.pipe(csslint({
+			'compatible-vendor-prefixes': false,
+			'box-sizing': false
+		}))
+		.pipe(csslint.reporter());
 });
 
-// Task that compiles scss files down to good old css
-
-gulp.task('sass', function(){
-  gulp.src('./sass/*.scss')
-      .pipe(watch(function(files) {
-        return files.pipe(sass({includePaths: ['./sass/']}))
-          .pipe(prefix())
-          .pipe(gulp.dest('./css/'))
-          .pipe(livereload(server));
-      }));
+gulp.task('prefix', function () {
+	gulp.src('./css/*.css')
+		.pipe(prefix())
+		.pipe(gulp.dest('./css/'));
 });
 
-// Task that compiles css using the myth processor
-
-gulp.task('myth', function(){
-  gulp.src('./myth/*.css')
-      .pipe(myth())
-      .pipe(prefix())
-      .pipe(gulp.dest('./css/'));
+gulp.task('minify', function () {
+	gulp.src('./css/*.css')
+		.pipe(minifyCSS({
+			keepSpecialComments: 0
+		}))
+		.pipe(gulp.dest('./css/'))
+		.pipe(rename('colors.min.css'))
+		.pipe(gulp.dest('./css/'));
 });
 
+// One task to run them all
+gulp.task('css', ['lint', 'prefix', 'minify']);
 
-// Just runs autoprefixer on the compiled css
+// JavaScript
 
-gulp.task('prefix', function(){
-  gulp.src('./css/*.css')
-      .pipe(prefix())
-      .pipe(gulp.dest('./css/'));
+
+// Anything to do here?
+
+
+// LESS
+
+// gulp.task('less', function () {
+// 	gulp.src('./less/')
+// });
+
+
+// Gotta check out LESS and see how to do it
+
+
+// Myth
+
+gulp.task('myth', function () {
+	gulp.src('./myth/*.css')
+		.pipe(myth())
+		.pipe(prefix())
+		.pipe(gulp.dest('./css/'));
 });
 
-gulp.task('stylus', function(){
-  gulp.src('./stylus/colors.styl')
-      .pipe(watch(function(files) {
-        return files.pipe(stylus())
-        .pipe(prefix())
-        .pipe(gulp.dest('./css/'))
-        .pipe(livereload(server));
-      }));
+// SASS
+
+gulp.task('sass', function () {
+	gulp.src('./sass/*.scss')
+		.pipe(watch(function (files) {
+			return files.pipe(sass({
+				includePaths: ['./sass/']
+			}))
+				.pipe(prefix())
+				.pipe(gulp.dest('./css/'))
+				.pipe(livereload(server));
+		}));
 });
 
-/*
-   DEFAULT TASK
+// Stylus
 
- • Process sass and lints outputted css
- • Outputted css is run through autoprefixer
- • Runs jshint on all js files in ./js/
- • Sends updates to any files in directory to browser for
- automatic reloading
-
-*/
-
-gulp.task('default', function(){
-  server.listen(35729, function (err) {
-    gulp.watch(['./sass/*.scss'], function(event) {
-      gulp.run('sass', 'lint');
-    });
-  });
+gulp.task('stylus', function () {
+	gulp.src('./stylus/colors.styl')
+		.pipe(watch(function (files) {
+			return files.pipe(stylus())
+				.pipe(prefix())
+				.pipe(gulp.dest('./css/'))
+				.pipe(livereload(server));
+		}));
 });
 
+/**
+ * DEFAULT TASK
+ * Process SASS and lint processed CSS
+ * processed CSS gets autoprefixed
+ * Run JSHint on all JS files
+ * LiveReload server autoreloads browser
+ */
+
+gulp.task('default', function () {
+	server.listen(35729, function (err) {
+		gulp.watch(['./sass/*.scss'], function (event) {
+			gulp.run('sass', 'lint');
+		});
+	});
+});
